283bf318d68497cd0cbbd2ca0e3355cf501dffe5
Keshopan Chelvan <kchelvan@vyana.io>
2023-08-26 16:25:28 -0400

    Included Screenshots and updated styling of Status Bar

--- App.tsx
+++ App.tsx
@@ -1,25 +1,29 @@
 import React from 'react';
-import {StatusBar, StyleSheet} from 'react-native';
-import {createStore, combineReducers, applyMiddleware} from 'redux';
-import {Provider} from 'react-redux';
+import { StatusBar, StyleSheet } from 'react-native';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
 import ReduxThunk from 'redux-thunk';
 
 import AppNavigator from './navigation/AppNavigator';
 import baseReducer from './store/reducers/base';
+import Colors from './constants/Colors';
 
 const rootReducer = combineReducers({
-  base: baseReducer,
+	base: baseReducer,
 });
 
 const store = createStore(rootReducer, applyMiddleware(ReduxThunk));
 
 function App(): JSX.Element {
-  return (
-    <Provider store={store}>
-      <StatusBar barStyle="light-content" />
-      <AppNavigator />
-    </Provider>
-  );
+	return (
+		<Provider store={store}>
+			<StatusBar
+				barStyle='light-content'
+				backgroundColor={Colors.Primary.background}
+			/>
+			<AppNavigator />
+		</Provider>
+	);
 }
 
 const styles = StyleSheet.create({});
